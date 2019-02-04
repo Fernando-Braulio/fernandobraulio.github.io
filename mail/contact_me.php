4e7143a559eb08b40ba69db25fc69fef58a85a12
Fernando-Braulio <fernandobraulio@outlook.com.br>
2019-02-04 17:57:56 -0200

    new portfolio

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,11 +11,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "teste@teste.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Website Contact Form:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
-$header = "From: teste@teste.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$header .= "Reply-To: $email";	
+$to = "fernandobraulio@outlook.com.br"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = "Website Contact FB:  $name";
+$body = "Você recebeu uma nova mensagem do formulário de contato do seu website.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email\n\nPhone: $phone\n\nMensagem:\n$message";
+$header = "From: fernandobraulio@outlook.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header .= "Responder a: $email";	
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
